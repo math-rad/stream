72dfe6492adeb499abaf3607413ded96c777728b
math-rad <ben.sir545@outlook.com>
2024-03-06 14:09:25 -0600

    Sync

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log(require("token.json"))
+const { Client, REST, GatewayIntentBits, EmbedBuilder, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, Routes } = DiscordJs
+
+const TOKEN = process.env.TOKEN;
+
+const radClient = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages]
+})
+
+console.log(TOKEN);
