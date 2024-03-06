4469a38e40a666ff1f5e3668f1aa2e5fc8191e1d
math-rad <ben.sir545@outlook.com>
2024-03-06 14:55:58 -0600

    erm

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 "use strict";
-const { Client, REST, GatewayIntentBits, EmbedBuilder, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, Routes } = require("discord.js");
-
-const TOKEN = process.env.TOKEN;
-
-const radClient = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages]
-})
-
-console.log(TOKEN);
+console.log(process.env.TOKEN);
