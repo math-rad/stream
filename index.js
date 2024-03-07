a06b958516d939565a6375253dbedcf7b3339867
math.rad <ben.sir545@outlook.com>
2024-03-07 10:54:43 -0600

    record/play audio logic / not finished

--- index.js
+++ index.js
@@ -1,21 +1,124 @@
-const {token} = require("./localconstants.json");
+const { token } = require("./localconstants.json");
 
 const discordJs = require("discord.js");
+const voice = require("@discordjs/voice")
 
-const {Client, EmbedBuilder, Intents} = discordJs;
+const { Client, EmbedBuilder, Intents } = discordJs;
 
-console.log(discordJs.IntentsBitField)
 
 const streamItClient = new Client({
     "intents": [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS]
 })
 
+const radicalStream = voice.createAudioPlayer({
+
+})
+
+
+class cache {
+    constructor() {
+        this.cache = {}
+    }
+
+    process() {
+
+    }
+
+    getKey() {
+
+    }
+
+    get(input, key) {
+        const key = key || this.key(input)
+        const cachedContent = this.cache[key]
+
+        if (cachedContent) {
+            return cachedContent
+        }
+
+        const content = process(input)
+        this.cache[key] = content
+
+        return content;
+    }
+}
+
+const audioResourceCache = new (class extends cache {
+    /**
+     * 
+     * @param {discordJs.MessageAttachment} attachment 
+     */
+    process(attachment) {
+        return voice.createAudioResource(attachment.url)
+    }
+
+
+    /**
+     * 
+     * @param {discordJs.MessageAttachment} attachment 
+     * @returns 
+     */
+    getKey(attachment, key) {
+        return attachment.url
+    }
+})
+
+const attachmentCache = new (class extends cache{
+    process(attachment) {
+        return attachment
+    }
+    getKey(attachment) {
+        return attachment.url;
+    }
+})
+
+/**
+ * 
+ * @param {discordJs.MessageAttachment} attachment 
+ */
+
+function makeAudioResource(attachment, key) {
+    return audioResourceCache.get(attachment.key)
+}
+
+
 streamItClient.on("message", (m) => {
     if (m.author.bot) {
         return
     }
-    m.reply({
-        files: ["./pipe.mp3"]
-    })
+
+    const content = m.content;
+
+    if (content.substring(0, 1) === '!') {
+        const arguments = content.substring(1).split('!')
+
+        switch (arguments[0]) {
+            case "record": {
+                const attachment = m.attachments.array()[0]
+                if (!attachment) {
+                    return
+                }
+
+                const name = arguments[1]
+                makeAudioResource(attachment, (name != '' && name != null) && name)
+                attachmentCache.get()
+
+                m.reply(`Recorded audio as ${name || attachment.url}`)
+                break
+            }
+
+            case "send": {
+                const attachment = audioResourceCache.cache[arguments[0]]
+                if (!attachment) {
+                    m.reply("no recorded value found")
+                    break
+                }
+
+                m.reply({
+                    "attachments": [attachment.url]
+                })
+            }
+        }
+    }
 })
 streamItClient.login(token)
